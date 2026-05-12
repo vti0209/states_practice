8855f30ceb2c841d0a526ad6d8bd311d5f397d2a
vti0209 <hotrai84@gmail.com>
2026-05-12 11:40:46 +0700

    add style bài 4: click button ra màu

--- src/components/Mausac.jsx
+++ src/components/Mausac.jsx
@@ -1,11 +1,11 @@
 import {useState} from 'react'
-// bài này là click button xanh ra nền xanh và click button đỏ ra nền đỏ
+// bài này là click button ra nền theo ý muốn
 export function Mausac() {
     const [color, setColor] = useState('white');
     return (
-        <div style={{ backgroundColor: color, height: '100vh' }}>
-            <button onClick={() => setColor('blue')}>Xanh</button>
-            <button onClick={() => setColor('red')}>Đỏ</button>
+        <div style={{ backgroundColor: color, height: '150px', width: '150px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+            <button onClick={() => setColor('purple')}>Tím</button>
+            <button onClick={() => setColor('green')}>Xanh lá</button>
         </div>
     );
 }
